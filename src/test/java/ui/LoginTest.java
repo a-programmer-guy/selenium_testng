1e1910c87000dbd24f325d3d42b1df7d9b25b324
a-programmer-guy <ctrl_kay@protonmail.com>
2022-08-19 16:49:22 -0600

    Feature -  Add DriverFactory class

--- src/test/java/ui/LoginTest.java
+++ src/test/java/ui/LoginTest.java
@@ -4,11 +4,11 @@ import org.testng.annotations.Test;
 
 public class LoginTest {
 	
-	@Test
+	@Test(priority = 1, description = "This is a login test")
 	public void loginTest() {
 		System.out.println("Login test successful...");
 	}
-
+	@Test(priority = 2, description = "This is a logout test")
 	public void logoutTest() {
 		System.out.println("Logoout test successful...");
 	}
