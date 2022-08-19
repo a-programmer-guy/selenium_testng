40952ac569451720b6beeec87ed76d93244e355f
a-programmer-guy <ctrl_kay@protonmail.com>
2022-08-19 16:19:16 -0600

    Initial Commit
    Add project structure and files

---
+++ src/test/java/ui/LoginTest.java
@@ -0,0 +1,15 @@
+package ui;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	@Test
+	public void loginTest() {
+		System.out.println("Login test successful...");
+	}
+
+	public void logoutTest() {
+		System.out.println("Logoout test successful...");
+	}
+}
